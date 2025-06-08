33a030bb8922f05fc02d87a3e92a774822936ed0
FranBeyer <beyer.fran@gmail.com>
2025-06-08 14:22:55 +0200

    Delete pages/index.js

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
-      <h1>Welcome to CastConnect!</h1>
-      <p>This is your home page.</p>
-    </div>
-  );
-}
