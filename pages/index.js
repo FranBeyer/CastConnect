356220056ca278407a72e9031fd3b48be4844c44
FranBeyer <beyer.fran@gmail.com>
2025-06-22 12:48:29 +0000

    Fix supabase redeclaration error

--- pages/index.js
+++ pages/index.js
@@ -77,12 +77,7 @@ export default function Home() {
   );
 }
 
-import { useEffect, useState } from 'react';
-import { createClient } from '@supabase/supabase-js';
 
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default function Home() {
