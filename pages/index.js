2e68a1a6ac5dfda08af8de2297ff51d2bfd8edb5
FranBeyer <beyer.fran@gmail.com>
2025-06-27 19:43:39 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 export default function Home() {
   return (
-    <div>
+    <main style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
       <h1>Welcome to CastConnect</h1>
-      <p>This is the homepage.</p>
-    </div>
+      <p>
+        This is your talent platform homepage. You're all set up and ready to build!
+      </p>
+    </main>
   );
 }
